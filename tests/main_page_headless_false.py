79136227f53a362eaa550e33e8ed3e5af65f5347
sungm1nn <jjsm4690@naver.com>
2025-12-13 16:23:36 +0900

    update headless

--- tests/main_page_headless_false.py
+++ tests/main_page_headless_false.py
@@ -4,7 +4,7 @@ import re
 
 def test_main_page() :
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
