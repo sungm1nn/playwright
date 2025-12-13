68eb06c62a76b15f2820c65ef4e3cf48db4c78e6
sungm1nn <jjsm4690@naver.com>
2025-12-13 16:03:20 +0900

    always succes (go to main page)

---
+++ tests/main_page.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright, expect
+from time import sleep
+import re
+
+def test_hamburger_button() :
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://app.3o3.co.kr/login")
+        expect(page).to_have_url("https://app.3o3.co.kr")
+
+
+if __name__ == "__main__":
+    test_hamburger_button()
